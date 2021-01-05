4f62049796e204e4195770542022c7cc7ded6908
StasDeep <glubokiy.stas@gmail.com>
2021-01-05 19:16:00 +0300

    2015-14

--- 2015/14/solution.py
+++ 2015/14/solution.py
@@ -5,10 +5,9 @@ from utils import read, p1, p2
 
 def main(is_test):
     seconds = 1000 if is_test else 2503
-    lines = read()
 
     m = {}
-    for line in lines:
+    for line in read():
         name, *vals = re.search(r'(.*) can fly (\d+)[^\d]+(\d+)[^\d]+(\d+)', line).groups()
         m[name] = list(map(int, vals))
 
@@ -17,10 +16,7 @@ def main(is_test):
     all_dists = {name: [calc(sec, *vals) for sec in range(1, seconds + 1)] for name, vals in m.items()}
     best_dists = [max(dists_for_sec) for dists_for_sec in zip(*all_dists.values())]
 
-    p2(max(
-        sum(dist == best_dist for dist, best_dist in zip(dists, best_dists))
-        for dists in all_dists.values()
-    ))
+    p2(max(sum(dist == best_dist for dist, best_dist in zip(dists, best_dists)) for dists in all_dists.values()))
 
 
 def calc(seconds, speed, time, rest):
