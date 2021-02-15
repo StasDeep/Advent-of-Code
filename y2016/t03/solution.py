c73a4fe81aff97cc4d20874615af5a85b6cc6a32
StasDeep <glubokiy.stas@gmail.com>
2021-02-15 17:56:18 +0300

    Reduce lines in 2016-03

--- y2016/t03/solution.py
+++ y2016/t03/solution.py
@@ -2,18 +2,10 @@ from utils import read, p1, p2
 
 
 def main():
-    lines = read()
-
-    triangles = [[int(x) for x in line.strip().split()] for line in lines]
-    p1(sum(is_possible_triangle(triangle) for triangle in triangles))
-
-    c = 0
-    for i in [0, 1, 2]:
-        for t1, t2, t3 in zip(triangles[::3], triangles[1::3], triangles[2::3]):
-            c += int(is_possible_triangle([t1[i], t2[i], t3[i]]))
-    p2(c)
+    a = [[int(x) for x in line.strip().split()] for line in read()]
+    p1(sum(is_valid(*t) for t in a))
+    p2(sum(is_valid(a[i], b[i], c[i]) for a, b, c in zip(a[::3], a[1::3], a[2::3]) for i in range(3)))
 
 
-def is_possible_triangle(lengths):
-    a, b, c = sorted(lengths)
-    return a + b > c
+def is_valid(*lengths):
+    return sum(sorted(lengths)[:2]) > max(lengths)
