9858756c743cf18a1743bff813e88a64824df0f5
Stanislau Hluboki <stanislau.hluboki@bolt.eu>
2022-12-06 06:22:04 +0100

    Perhaps this would be better lol

--- y2022/t06/solution.py
+++ y2022/t06/solution.py
@@ -2,10 +2,13 @@ from utils import read, p1, p2
 
 
 def main():
-    lines = read()[0]
+    line = read()[0]
 
     for n, func in [(4, p1), (14, p2)]:
-        for i, x in enumerate(zip(*[lines[j:] for j in range(n)])):
-            if len(set(x)) == n:
+        # for i, x in enumerate(zip(*[line[j:] for j in range(n)])):
+        #     if len(set(x)) == n:
+        #         break
+        for i in range(len(line)):
+            if len(set(line[i:i+n])) == n:
                 break
         func(i+n)
