52286b34a762eb3e3133a6faa85bac691a016269
Stanislau Hluboki <stanislau.hluboki@bolt.eu>
2022-12-06 06:20:39 +0100

    Refactor

--- y2022/t06/solution.py
+++ y2022/t06/solution.py
@@ -1,16 +1,11 @@
 from utils import read, p1, p2
-import re
 
 
 def main():
     lines = read()[0]
 
-    for i, x in enumerate(zip(lines, lines[1:], lines[2:], lines[3:])):
-        if len(set(x)) == 4:
-            break
-    p1(i+4)
-
-    for i, x in enumerate(zip(*[lines[j:] for j in range(14)])):
-        if len(set(x)) == 14:
-            break
-    p2(i+14)
+    for n, func in [(4, p1), (14, p2)]:
+        for i, x in enumerate(zip(*[lines[j:] for j in range(n)])):
+            if len(set(x)) == n:
+                break
+        func(i+n)
