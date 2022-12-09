f275f84eebefbfaac9b54716f15d1dee64e51785
Stanislau Hluboki <stanislau.hluboki@bolt.eu>
2022-12-09 07:24:05 +0100

    Small refactor

--- y2022/t09/solution.py
+++ y2022/t09/solution.py
@@ -1,32 +1,19 @@
-from collections import defaultdict
-
 from utils import read, p1, p2
+
+DIRS = {'R': [0, 1], 'L': [0, -1], 'U': [-1, 0], 'D': [1, 0]}
 
 
 def main():
     lines = read()
 
     for rope_len, func in [(2, p1), (10, p2)]:
-        d = defaultdict(bool)
-        coords = [
-            [0, 0]
-            for _ in range(rope_len)
-        ]
-        d[tuple(coords[-1])] = True
+        d = {}
+        coords = [[0, 0] for _ in range(rope_len)]
         for instr in lines:
             c, n = instr.split()
             n = int(n)
-            if c == 'R':
-                di = [0, 1]
-            if c == 'L':
-                di = [0, -1]
-            if c == 'U':
-                di = [-1, 0]
-            if c == 'D':
-                di = [1, 0]
-
             for j in range(n):
-                coords[0] = [coords[0][0] + di[0], coords[0][1] + di[1]]
+                coords[0] = [coords[0][0] + DIRS[c][0], coords[0][1] + DIRS[c][1]]
                 for i in range(rope_len - 1):
                     hc = coords[i]
                     tc = coords[i+1]
